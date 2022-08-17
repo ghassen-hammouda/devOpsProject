db1c04c2e68e11ea31a5e680172d6482412aa891
ghassen-hammouda <81829678+ghassen-hammouda@users.noreply.github.com>
2022-08-17 13:58:36 +0200

    prject commit

---
+++ src/main/java/tn/medicalDeal/tic/DevOpsProjectApplication.java
@@ -0,0 +1,13 @@
+package tn.medicalDeal.tic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/medicalDeal/tic/DevOpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.medicalDeal.tic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
